d1d021fef2a6e4c9665628ea7c8fae4d35121604
MathiMathu <thusha2024@gmail.com>
2020-09-20 06:59:32 +0530

    merged and added

---
+++ CscExamples.java
@@ -0,0 +1,53 @@
+public class CscExamples
+{
+ int questionNo;
+ String category;
+ String text;
+
+ public int getQuestionNo()
+ {
+   return questionNo;
+ }
+
+ public String getCategory()
+ {
+   return category;
+ }
+
+ public String getText()
+ {
+   return text;
+ }
+
+ public void setQuestionNo(int a)
+ {
+   questionNo = a;
+ }
+
+ public void setCategory(String b)
+ {
+   category = b;
+ }
+
+ public void setText(String c)
+ {
+   text=c;
+ }
+
+ public void addQuestion(CscExamples qu)
+ {
+   setQuestionNo(qu.getQuestionNo());
+   setText(qu.getText());
+   setCategory(qu.getCategory());
+ }
+
+ public void removeQuestion(int quId)
+ {
+
+ }
+
+ public void editQuestion(int quId)
+ {
+
+ }
+} 
